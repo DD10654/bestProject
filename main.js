4b711631f7ed42c338298acb0aa56da72ced401d
DD10654 <75024070+DD10654@users.noreply.github.com>
2021-08-28 20:18:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
-    video.parent("video_container");
 
-    canvas = createCanvas(550, 500);
-    canvas.position(175, 35)
-    canvas.parent("canvas_container");
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 170);
 
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on('pose', gotposes)
@@ -13,6 +17,11 @@ function setup() {
 
 function draw() {
     background('#492839');
+    document.getElementById("w7h").innerHTML = "The Width and Height Of Square is " + difference + " Pixels";
+    fill("powderblue");
+    stroke("darkslategrey");
+    strokeWeight(5)
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -22,5 +31,13 @@ function modelLoaded() {
 function gotposes(result) {
     if (result.length > 0) {
         console.log(result);
+        noseX = result[0].pose.nose.x;
+        noseY = result[0].pose.nose.y;
+        console.log("Nose X is " + noseX + " and Nose Y is " + noseY);
+
+        leftWristX = result[0].pose.leftWrist.x;
+        rightWristX = result[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X is " + leftWristX + " and Right Wrist X is " + rightWristX + " And So the Difference is " + difference);
     }
 }
